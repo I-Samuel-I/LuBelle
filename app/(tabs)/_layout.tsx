d59b724c6a5f22c475e9fa42374efd849d45f1c3
Samuel Gomes <SamPongPie@gmail.com>
2025-08-14 11:25:30 -0300

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -12,6 +12,7 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
+    <>
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
@@ -33,13 +34,8 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+    
+    </Tabs> 
+    </>
   );
 }
---
+++ app/(tabs)/style.tsx
@@ -0,0 +1,9 @@
+import {StyleSheet} from "react-native"
+
+const mainContainer = StyleSheet.create({
+    test:{
+        color: "#ffff"
+    }
+})
+
+export default mainContainer;
